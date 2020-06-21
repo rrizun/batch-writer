155840122e56297fe2baaaa0444dc45a6924e520
rrizun <rrizun>
2020-06-21 15:32:55 -0700

    logback

--- src/main/java/helpers/LogHelper.java
+++ src/main/java/helpers/LogHelper.java
@@ -2,23 +2,40 @@ package helpers;
 
 import java.util.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * LogHelper
  */
 public class LogHelper {
-  private final Object object;
+  private final Logger logger;
 
   public LogHelper(Object object) {
-    this.object = object;
+    logger = LoggerFactory.getLogger(object.getClass());
   }
 
   public void log(Object... args) {
+    logger.info(str(args));
+  }
+
+  public void debug(Object... args) {
+    logger.debug(str(args));
+  }
+
+  public void trace(Object... args) {
+    logger.trace(str(args));
+  }
+
+  public String str(Object... args) {
     List<String> parts = new ArrayList<>();
-    parts.add(new Date().toString());
-    parts.add(object.toString());
     for (Object arg : args)
       parts.add("" + arg);
-    System.out.println(String.join(" ", parts));
+    return String.join(" ", parts);
   }
+
+  // static {
+  //   ((ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger("ROOT")).setLevel(ch.qos.logback.classic.Level.INFO);
+  // }
 
 }
